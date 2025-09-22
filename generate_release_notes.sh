ad93c742f8b0f6d8f5f096ea86b0ba4ab3624197
firmsoil <firmsoil@gmail.com>
2025-09-22 00:08:25 -0700

    Update generate_release_notes.sh

--- generate_release_notes.sh
+++ generate_release_notes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Make executable: chmod +x generate_release_notes.sh
 
-REPO="firmsoil/slsa"
-FROM_TAG="v1.0-rc2"
-TO_TAG="v1.0.0"
+REPO="${REPO:-firmsoil/slsa}"
+FROM_TAG="${FROM_TAG:-v0.1.0}"
+TO_TAG="${TO_TAG:-v1.0.0}"
 
 generate-release-notes --repo "$REPO" --from-tag "$FROM_TAG" --to-tag "$TO_TAG"
 echo "Release notes generated in release_notes.txt"
